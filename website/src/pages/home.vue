d4c97f99bebc673a37e0432508b082454af827fd
顾世豪 <gushihao@worken.cn>
2018-07-16 15:09:13 +0800

    编写首页

---
+++ website/src/pages/home.vue
@@ -0,0 +1,19 @@
+
+<style scoped>
+
+</style>
+
+<template>
+    <div>
+        <Layout>
+            <Header>Header</Header>
+            <Content>Content</Content>
+        </Layout>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    };
+</script>
--- website/src/router/index.js
+++ website/src/router/index.js
@@ -1,15 +1,16 @@
+
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import homePage from '@/pages/home'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: homePage
+        }
+    ]
 })
