6620058fa5140d9be19183569dead975a5ae90e2
顾世豪 <gushihao@worken.cn>
2018-07-18 12:26:30 +0800

    中午提交一下

--- server/src/index.js
+++ server/src/index.js
@@ -9,16 +9,27 @@ const config = {
     user: "sa",
     password: "gushihao",
     server: "localhost",
-    database: "QQGroup"
+    database: "QQGroup",
 };
 
 const serverPort = 10241;
+
+function hold404 (ctx, next) {
+    let code = 404;
+    ctx.status = code;
+    ctx.body = {
+        code: code,
+        data: null,
+        msg: "资源不存在",
+    };
+}
 
 async function main () {
     try {
         let pool = await MSSQL.connect(config);
         let app = new Koa();
         let router = new KoaRouter();
+
         //服务端跨域配置
         let cors = Koa2Cors({
             origin: function (ctx) {
@@ -32,45 +43,67 @@ async function main () {
         });
 
         //QQ号拓展查询接口
-        router.get('/qqext/:id', async (ctx, next) => {
+        router.get('/api/qqext/:id', async (ctx, next) => {
             let qqNum = Number(ctx.params.id);
             let result = await pool.request()
                             .input('QQNum', MSSQL.Int, qqNum)
                             .execute('queryByQQNumExt');
+            let code = 200;
+            ctx.status = code;
             ctx.body = {
-                member: result.recordsets[0],
-                group: result.recordsets[1],
-                link: result.recordsets[2]
+                code: code,
+                data: {
+                    member: result.recordsets[0],
+                    group: result.recordsets[1],
+                    link: result.recordsets[2],
+                },
+                msg: "查询成功",
             };
         });
 
         //QQ号查询接口
-        router.get('/qq/:id', async (ctx, next) => {
+        router.get('/api/qq/:id', async (ctx, next) => {
             let qqNum = Number(ctx.params.id);
             let result = await pool.request()
                             .input('QQNum', MSSQL.Int, qqNum)
                             .execute('queryByQQNum');
+            let code = 200;
+            ctx.status = code;
             ctx.body = {
-                member: result.recordsets[0],
-                group: result.recordsets[1],
-                link: result.recordsets[2]
+                code: code,
+                data: {
+                    member: result.recordsets[0],
+                    group: result.recordsets[1],
+                    link: result.recordsets[2],
+                },
+                msg: "查询成功",
             };
         });
 
         //群号查询接口
-        router.get("/group/:num", async (ctx, next) => {
+        router.get("/api/group/:num", async (ctx, next) => {
             let groupNum = Number(ctx.params.num);
             let result = await pool.request()
                             .input("groupNum", MSSQL.Int, groupNum)
                             .execute("queryByGroupNum");
+            let code = 200;
+            ctx.status = code;
             ctx.body = {
-                member: result.recordsets[0],
-                group: result.recordsets[1],
-                link: result.recordsets[2]
+                code: code,
+                data: {
+                    member: result.recordsets[0],
+                    group: result.recordsets[1],
+                    link: result.recordsets[2],
+                },
+                msg: "查询成功",
             };
         });
 
-        app.use(cors).use(KoaStatic(__dirname + "/www")).use(router.routes()).use(router.allowedMethods());
+        app
+            .use(cors)
+            .use(KoaStatic(__dirname + "/www"))
+            .use(router.routes())
+            .use(hold404);
 
         app.listen(serverPort);
         console.log(`服务已经启动，工作在 ${ serverPort } 端口...`);
